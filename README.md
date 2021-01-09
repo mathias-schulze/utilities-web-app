d442de9bd364a36123c6a7437f6152ae2b39fa66
Mathias Schulze <mathias-schulze@web.de>
2021-01-10 00:09:01 +0100

    Revert "create react app (Redux-Toolkit + Typescript)"
    This reverts commit b053a43d52f96eb572354c770742fbcb7a697248.

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # utilities-web-app
-
-## bootstrapped with create-react-app template for Redux-Toolkit with Typescript
-```bash
-npx create-react-app utilities-web-app --template toolkit
-```
--- src/store/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import todosReducer from './todos';
-
-const rootReducer = combineReducers({
-  todos: todosReducer
-});
-
-export default rootReducer;
